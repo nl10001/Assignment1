1e1bad0a2bafe854fb9fb0f0da00d74d74e4be42
nl10001 <98903190+nl10001@users.noreply.github.com>
2022-02-03 16:06:08 +0000

    Adding program structure

--- Assignment1.ino
+++ Assignment1.ino
@@ -8,40 +8,41 @@
 */
 
 #include<stdio.h>
+#inclue<time.h>
 
 // defining the corresponding pins to the ESP32 board 
-const int ledPinA = 15;
-const int ledPinB = 21;
-const int buttonA = 22;
-const int buttonB = 23;
+const int ledPin1 = 15;
+const int ledPin2 = 21;
+const int button1 = 22;
+const int button2 = 23;
 
-int buttonStateA = 0;
-int buttonStateB = 0;
+int buttonState1 = 0;
+int buttonState2 = 0;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(buttonA, INPUT);
-  pinMode(buttonB, INPUT);
-  pinMode(ledPinA, OUTPUT);
-  pinMode(ledPinB, OUTPUT);
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  buttonStateA = digitalRead(buttonA);
-  buttonStateB = digitalRead(buttonB);
+  buttonState1 = digitalRead(button1);
+  buttonState2 = digitalRead(button2);
+  
+  if(button1 == LOW){ // enable waveform
+    if(button2 == LOW) { // normal waveform
 
-  if(buttonStateA == HIGH ) {
-    digitalWrite(ledPinA, HIGH);
-    digitalWrite(ledPinB, LOW);
+    }
+    else if(button2 == HIGH) { // inverted waveform
+      
+    }
   }
-  else if(buttonStateB == HIGH) {
-    digitalWrite(ledPinB, HIGH);
-    digitalWrite(ledPinA, LOW);
+  else if(button1 == HIGH) { // disable waveform
+
   }
-  else{
-    digitalWrite(ledPinA, LOW);
-    digitalWrite(ledPinB, LOW);
-  }
+  
  }
